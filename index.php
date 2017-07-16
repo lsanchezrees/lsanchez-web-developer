8d51ace3e6d4ec0a08e049ce0a89a9dac0922022
Leonora Sanchez-Rees <lsanchezrees@cnm.edu>
2017-07-16 11:02:42 -0600

    Created index file for future personal website

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Leonora
- * Date: 7/13/2017
- * Time: 7:23 PM
- */
+<!doctype html>
+<html>
+	<head>
+		<title>Leonora Sanchez, Web Developer</title>
+	</head>
+	<body>
+		<h1>Future file for personal website</h1>
+		<p>
+
+		</p>
+	</body>
+</html>
