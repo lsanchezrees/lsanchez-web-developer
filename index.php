77087d537469e3e5f98ef5f09bdcc67b165aba7f
Leonora Sanchez-Rees <lsanchezrees@cnm.edu>
2017-07-16 11:00:40 -0600

    Created new file for milestone 1

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Leonora
+ * Date: 7/13/2017
+ * Time: 7:23 PM
+ */
---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+	<head>
+		<title>Milestone 1</title>
+	</head>
+	<body>
+		<h1>Milestone 1</h1>
+		<p>
+
+		</p>
+	</body>
+</html>
