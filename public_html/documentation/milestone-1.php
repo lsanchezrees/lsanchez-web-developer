986a2420d0b7d8b0afea2e2ea9d606f339ee375f
Leonora Sanchez-Rees <lsanchezrees@cnm.edu>
2017-07-16 11:08:29 -0600

    Added purpose, audience and goal

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -5,8 +5,17 @@
 	</head>
 	<body>
 		<h1>Milestone 1</h1>
-		<p>
+		<h2>Purpose, Audience, Goal</h2>
+		<h3>Purpose</h3>
+		<p>The purpose of my website is to show my skills to potential employeers, to make my information accessible, and to serve as an introduction to procure employment.</p>
+		<h3>Audience</h3>
+		<p>My website is designed to attract potential employeers</p>
+		<h3>Goal</h3>
+		<p>My goal is to obtain employment</p>
 
-		</p>
+		<h2>Persona</h2>
+		<p></p>
+		<h2>Use Case</h2>
+		<p></p>
 	</body>
 </html>
