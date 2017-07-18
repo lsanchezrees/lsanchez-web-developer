ef78ad608ec5878570d0fc93980bb10ce17fde59
Leonora Sanchez-Rees <lsanchezrees@cnm.edu>
2017-07-18 17:46:12 -0600

    Updates to persona

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -5,16 +5,21 @@
 	</head>
 	<body>
 		<h1>Milestone 1</h1>
-		<h2>Purpose, Audience, Goal</h2>
-		<h3>Purpose</h3>
-		<p>The purpose of my website is to show my skills to potential employeers, to make my information accessible, and to serve as an introduction to procure employment.</p>
-		<h3>Audience</h3>
-		<p>My website is designed to attract potential employeers</p>
-		<h3>Goal</h3>
-		<p>My goal is to obtain employment</p>
+
+		<p>Purpose:  The purpose of my website is to show my skills to potential employeers, to make my information accessible, and to serve as an introduction to procure employment.</p>
+		<p>Audience: My website is designed to attract potential employeers</p>
+		<p>Goal: My primary goal is to obtain employment as a web developer.  I want the site to attract potential
+			employers and also show them my capabilities.</p>
 
 		<h2>Persona</h2>
-		<p></p>
+		<p>Name:  Mike Manager</p>
+		<p>Age:  45</p>
+		<p>Profession:  Project Manager for a fairly large company.  The company does not specialize in website development, but rather uses in-house developers.</p>
+		<p>Technology:  Like most corporations in the business world, they use desktop PCs running windows.</p>
+		<p>Attitudes and Behaviors:  Mike is concerned with finding an employee who is a good fit for the company.</p>
+		<p>Frustrations and Needs:  </p>
+		<p>Goals: </p>
+		<p>User Story: </p>
 		<h2>Use Case</h2>
 		<p></p>
 	</body>
