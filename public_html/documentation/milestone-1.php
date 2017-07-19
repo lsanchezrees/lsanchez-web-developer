bf7427d4b688f8baa540f17ab98bf994c024789c
Leonora Sanchez-Rees <lsanchezrees@cnm.edu>
2017-07-19 10:09:51 -0600

    updates to styling

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -6,20 +6,20 @@
 	<body>
 		<h1>Milestone 1</h1>
 
-		<p>Purpose:  The purpose of my website is to show my skills to potential employeers, to make my information accessible, and to serve as an introduction to procure employment.</p>
-		<p>Audience: My website is designed to attract potential employeers</p>
-		<p>Goal: My primary goal is to obtain employment as a web developer.  I want the site to attract potential
-			employers and also show them my capabilities.</p>
+		<p><strong>Purpose:</strong>  The purpose of my website is to show my skills to potential employeers, to make my information accessible, and to serve as an introduction to procure employment.</p>
+		<p><strong>Audience:</strong> My website is designed to attract potential employeers</p>
+		<p><strong>Goal:</strong> My primary goal is to obtain employment as a full stack web developer.  I want the site to attract potential
+			employers and also show them my skills so that they can tell from my website if I have the skills they need.  I would also like to showcase my design skills as an additional bonus.</p>
 
 		<h2>Persona</h2>
-		<p>Name:  Mike Manager</p>
-		<p>Age:  45</p>
-		<p>Profession:  Project Manager for a fairly large company.  The company does not specialize in website development, but rather uses in-house developers.</p>
-		<p>Technology:  Like most corporations in the business world, they use desktop PCs running Windows.</p>
-		<p>Attitudes and Behaviors:  Mike is concerned with finding an employee who is a good fit for the company.   It's important to him that he makes a good decision with somewhat limited time.</p>
-		<p>Frustrations and Needs:  He needs someone who knows the technical side of development and is familiar with as many of the tools as possible.  He would prefer familiarity with 100% of the tools but knows that may be difficult to find and is willing to settle for less than 100%.  The second major characteristic he needs is someone who can work on a team with coworkers and who can work with customers.</p>
-		<p>Goals: Mike's main goal is to hire an employee who can do the work and will be a good fit for the company.  He needs someone who can fit into their corporate culture.</p>
-		<p>User Story: Mike needs to determine whether the candidate is a potential fit for the company, which means evaluating both technical skills and personality.</p>
+		<p><strong>Name:</strong>  Mike Manager</p>
+		<p><strong>Age:</strong>  45</p>
+		<p><strong>Profession:</strong>  Project Manager for a fairly large company.  The company does not specialize in website development, but rather uses in-house developers.</p>
+		<p><strong>Technology:</strong>  Like most corporations in the business world, they use desktop PCs running Windows.</p>
+		<p><strong>Attitudes and Behaviors:</strong>  Mike is concerned with finding an employee who is a good fit for the company.   It's important to him that he makes a good decision with somewhat limited time.</p>
+		<p><strong>Frustrations and Needs:</strong>  He needs someone who knows the technical side of development and is familiar with as many of the tools as possible.  He would prefer familiarity with 100% of the tools but knows that may be difficult to find and is willing to settle for less than 100%.  The second major characteristic he needs is someone who can work on a team with coworkers and who can work with customers.</p>
+		<p><strong>Goals:</strong> Mike's main goal is to hire an employee who can do the work and will be a good fit for the company.  He needs someone who can fit into their corporate culture.</p>
+		<p><strong>User Story:</strong> Mike needs to determine whether the candidate is a potential fit for the company, which means evaluating both technical skills and personality.</p>
 		<h2>Use Case</h2>
 		<p>Mike as received my application, resume and cover letter and based on my experience and skills has decided to schedule an initial interview.</p>
 		<p>It is 10:00 am and Mike is in his office.  He wants to use this initial interview to determine if my personality is a good fit for the company.  If he feels that I am, he will then have current members of the team interview me to evaluate my skills. Although Mike is a former coder, he is not that familiar with web coding and he wants the input from his team.</p>
