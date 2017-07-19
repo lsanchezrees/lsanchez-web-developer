a16563e062f4b7b283068f8bc237e2f7604d0a1e
Leonora Sanchez-Rees <lsanchezrees@cnm.edu>
2017-07-19 10:01:37 -0600

    updates to use case

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -18,9 +18,11 @@
 		<p>Technology:  Like most corporations in the business world, they use desktop PCs running Windows.</p>
 		<p>Attitudes and Behaviors:  Mike is concerned with finding an employee who is a good fit for the company.   It's important to him that he makes a good decision with somewhat limited time.</p>
 		<p>Frustrations and Needs:  He needs someone who knows the technical side of development and is familiar with as many of the tools as possible.  He would prefer familiarity with 100% of the tools but knows that may be difficult to find and is willing to settle for less than 100%.  The second major characteristic he needs is someone who can work on a team with coworkers and who can work with customers.</p>
-		<p>Goals: </p>
-		<p>User Story: </p>
+		<p>Goals: Mike's main goal is to hire an employee who can do the work and will be a good fit for the company.  He needs someone who can fit into their corporate culture.</p>
+		<p>User Story: Mike needs to determine whether the candidate is a potential fit for the company, which means evaluating both technical skills and personality.</p>
 		<h2>Use Case</h2>
-		<p></p>
+		<p>Mike as received my application, resume and cover letter and based on my experience and skills has decided to schedule an initial interview.</p>
+		<p>It is 10:00 am and Mike is in his office.  He wants to use this initial interview to determine if my personality is a good fit for the company.  If he feels that I am, he will then have current members of the team interview me to evaluate my skills. Although Mike is a former coder, he is not that familiar with web coding and he wants the input from his team.</p>
+		A use case should be based on your persona’s motivations and goals. Your use case should detail the time, location, technology used, and the purpose or goal of your Persona's use of your site.
 	</body>
 </html>
