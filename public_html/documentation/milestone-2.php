873faafaefebba963ba0d92b3dc8681641f5d09f
Leonora Sanchez-Rees <lsanchezrees@cnm.edu>
2017-07-29 15:10:16 -0600

    Created milestone 2 file

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+	<head>
+		<title>Milestone 2</title>
+	</head>
+	<body>
+		<h1>Milestone 2a</h1>
+		<h2>Content Strategy</h2>
+		<p>I plan to have four sections, as follows</p>
+		<ul>
+			<li>About me (as a developer)</li>
+			<li>Skills</li>
+			<li>Experience</li>
+			<li>Interests/hobbies</li>
+		</ul>
+		<p>Below that will be the contact form</p>
+		<p>The "about me" section will focus on my interests as a developer.  The skills section will list the skills I have or have learned.  The experience section will talk about previous experience, including IT experience before web design.  The personal section will some information about my interests and hobbies.   Finally, below the four sections will be a contact form.</p>
+
+		<h2>Wireframes</h2>
+
+	</body>
+</html>
