117089d2f7813ddaab745f6d9e4a0cd66232f5d9
Leonora Sanchez-Rees <lsanchezrees@cnm.edu>
2017-07-31 16:48:09 -0600

    updated milestone 2

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -6,12 +6,13 @@
 	<body>
 		<h1>Milestone 2a</h1>
 		<h2>Content Strategy</h2>
-		<p>I plan to have four sections, as follows</p>
+		<p>I plan to have one page with five sections, as follows.</p>
 		<ul>
 			<li>About me: This will focus on my interests as a developer.</li>
 			<li>Skills:  This will list skills I've learned or have from other jobs in the past.</li>
 			<li>Experience:  the experience section will talk about previous experience, including IT experience before web design.</li>
-			<li>Interests/hobbies:  this will be more about my personal interests and hobbies.</li>
+			<li>Interests/hobbies:  this will be more about my personal interests and hobbies.  This will have a background image.</li>
+			<li>Contact form:  This will be a contact form with a space for name, email, and comments.</li>
 		</ul>
 		<p>Finally, below the four sections will be the contact form</p>
 
