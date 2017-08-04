f280a20d925e14acf22d3783b8e763b37b9cdcf1
Leonora Sanchez-Rees <lsanchezrees@cnm.edu>
2017-08-04 10:35:04 -0600

    pushed jpgs

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -18,6 +18,11 @@
 
 
 		<h2>Wireframes</h2>
+		<h3>Mobile:</h3>
+		<img src="images/website-mockup-mobile.jpg">
+		<h3>Desktop:</h3>
+		<img src="images/website-mockup-desktop.jpg">
+
 
 	</body>
 </html>
