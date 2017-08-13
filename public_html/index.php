22850e1659a27d28f0886175405c4baac95a3a9e
Leonora Sanchez-Rees <leonora621@yahoo.com>
2017-08-13 11:46:13 -0600

    began web page

---
+++ public_html/index.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html>
+	<head>
+
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+		<!-- Font Awesome -->
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
+		<!-- custom css link -->
+		<link rel="stylesheet" type="text/css" href="css/styles.css">
+
+
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!--[if lt IE 9]>
+		<script type="text/javascript" src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script type="text/javascript" src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!-- jquery v3.0 -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+
+		<title>Leonora Sanchez, Web Developer</title>
+	</head>
+	<body>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6">
+					<h2>Leonora Sanchez-Rees, Web Developer</h2>
+					<h4>My focus as a web developer</h4>
+					<p>I am trained as a full stack web developer.  I have studied web design and have recently completed a Deep Dive Coding Bootcamp in order to gain experience in all aspects of web development.
+					</p>
+
+				</div><!--column-->
+				<div class="col-md-6">
+
+				</div><!--column-->
+			</div><!--row-->
+			<div class="row">
+				<div class="col-md-6">
+
+				</div><!--column-->
+				<div class="col-md-6">
+
+				</div><!--column-->
+			</div><!--row-->
+			<!--form goes here-->
+		</div><!--container-->
+	</body>
+</html>
