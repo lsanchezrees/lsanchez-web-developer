917560b6d348682cbbbd3cfbccfc2015ead95a26
Leonora Sanchez-Rees <leonora621@yahoo.com>
2017-08-22 21:16:29 -0600

    updated text

--- public_html/index.php
+++ public_html/index.php
@@ -44,7 +44,7 @@
 					<h3 class="text-center">Leonora Sanchez-Rees</h3>
 					<h4 class="text-center">Web Developer</h4>
 					<h5 class="text-center">My focus as a web developer</h5>
-					<p>I am trained as a full stack web developer.  I have a certificated in Media Arts with an emphasis in web design.  I have recently completed a Deep Dive Coding Bootcamp in order to gain experience in all aspects of web development.
+					<p>I am trained as a full stack web developer.  I have a certificate in Media Arts with an emphasis in web design.  I have recently completed a Deep Dive Coding Bootcamp in order to gain experience in all aspects of web development.
 					</p>
 
 				</div><!--column-->
@@ -64,14 +64,14 @@
 			</div><!--row-->
 			<div class="row">
 				<div class="col-sm-6 squares off-white">
-					<h4 class="text-center">Experience</h4>
-					<p>I have a background in programming with extensive database experience and experience in data conversion.  I went back to school to update my skills.  I have led projects, created applications.  While doing this, I have worked on many teams, and trained and supported customers extensively.</p>
+					<h4 class="text-center">Experience and Education</h4>
+					<p>I have a Master's degree in Linguistics as well as an A.A.S. in Information Systems.  I have a background in programming with extensive database experience and experience in data conversion.  I have led projects, created applications.  While doing this, I have worked on many teams, and trained and supported customers extensively. I returned to school to update my skills and am now certified in web development. </p>
 
 
 				</div><!--column-->
 				<div class="col-sm-6 squares lower-right">
 					<h4 class="text-center">Interests and Hobbies</h4>
-					<p>I live in Santa Fe with my husband, and we both love hiking.  When I'm not outdoors or with my dog, I love to read and solve puzzles.  I have an interest in nutrition and health.</p>
+					<p>I live in Santa Fe with my husband and dog.  I love to hike in all the beautiful wilderness around Santa Fe.  When I'm not outdoors or with my family, I love to read and solve puzzles.  I have an avid interest in nutrition and health.</p>
 
 
 
