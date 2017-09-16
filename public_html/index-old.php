e5137b6bf57c3fb794d91c01cb034acc2bccf98d
Leonora Sanchez-Rees <leonora621@yahoo.com>
2017-09-16 12:47:57 -0600

    changed to new website design, moved old

---
+++ public_html/index-old.php
@@ -0,0 +1,202 @@
+<!doctype html>
+<html>
+	<head>
+
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+		<!-- Font Awesome -->
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
+		<!-- custom css link -->
+		<link rel="stylesheet" type="text/css" href="css/styles.css">
+
+
+
+		<!--font links -->
+
+
+		<link href="https://fonts.googleapis.com/css?family=Courgette" rel="stylesheet">
+
+
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!--[if lt IE 9]>
+		<script type="text/javascript" src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script type="text/javascript" src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!-- jquery v3.0 -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+		<!-- jQuery Form, Additional Methods, Validate -->
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.min.js"></script>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js"></script>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/additional-methods.min.js"></script>
+
+		<!-- Your JavaScript Form Validator -->
+		<script src="js/form-validate.js"></script>
+
+		<!-- Google reCAPTCHA -->
+		<script src='https://www.google.com/recaptcha/api.js'></script>
+
+
+		<title>Leonora Sanchez, Web Developer</title>
+	</head>
+	<body class="sfooter">
+		<div class="sfooter-content">
+		<!--added header for external links-->
+		<header>
+			<nav class="navbar navbar-default">
+				<div class="container-fluid">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand" href="#">Leonora Sanchez-Rees</a>
+					</div>
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						<ul class="nav navbar-nav navbar-right">
+							<li><a href="https://www.linkedin.com/in/leonora-sanchez-815aaa104/" target="_blank"><i class="fa fa-linkedin-square" aria-hidden="true" ></i></a></li>
+							<li><a href="https://github.com/lsanchezrees" target="_blank"><i class="fa fa-github" aria-hidden="true"></i></a></li>
+							<li><a href="docs/lsanchezrees-resume.pdf" target="_blank">Resum&#233;</a></li>
+						</ul>
+					</div><!-- /.navbar-collapse -->
+				</div><!-- /.container-fluid -->
+			</nav>
+		</header>
+
+		<h2 class="text-center display-font1">Leonora Sanchez-Rees, Web Developer</h2>
+		<div class="container bg-container">
+
+			<div class="row">
+				<div class="col-sm-6 squares upper-left">
+
+					<h3 class="text-center">My focus as a web developer</h3>
+					<p>I am trained as a full stack web developer.  I have a certificate in Media Arts with an emphasis in web design.  I have recently completed a Deep Dive Coding Bootcamp in order to gain experience in all aspects of web development.
+					</p>
+
+				</div><!--column-->
+				<div class="col-sm-6 squares off-white">
+					<div class="row"><!--row for header-->
+					<div class="col-sm-12">
+					<h3 class="text-center">Skills</h3>
+					</div><!--column-->
+					</div><!--header row -->
+					<div class="row">
+						<div class="col-xs-6 col-xs-12">
+					<ul>
+						<li>mySQL</li>
+						<li>HTML and CSS</li>
+						<li>Bootstrap</li>
+						<li>JavaScript</li>
+						<li>jQuery</li>
+						<li>PHP</li></ul></div><!--first column of skills-->
+						<div class="col-xs-6 col-xs-12">
+						<ul><li>Angular4</li>
+						<li>WordPress</li>
+						<li>Adobe Photoshop</li>
+						<li>Adobe Illustrator</li>
+						<li>Adobe InDesign</li>
+						</ul>
+						</div><!--2nd column of skills-->
+					</div><!--row-->
+
+				</div><!--column-->
+			</div><!--row-->
+			<div class="row">
+				<div class="col-sm-6 squares off-white">
+					<h3 class="text-center">Experience and Education</h3>
+					<p>I have a Master's degree in Linguistics as well as an A.A.S. in Information Systems.  I have a background in programming with extensive database experience and experience in data conversion.  I have studied and/or worked in Sybase, Oracle and mySQL and am familiar with MSAccess and MongoDB.  As a programmer/analyst, I have led projects, created applications.  While doing this, I have worked on many teams, and trained and supported customers extensively. I returned to school to update my skills and am now certified in web development. </p>
+
+
+				</div><!--column-->
+				<div class="col-sm-6 squares lower-right">
+					<h3 class="text-center">Interests and Hobbies</h3>
+					<p>I live in Santa Fe with my husband and dog.  I love to hike in all the beautiful wilderness around Santa Fe.  When I'm not outdoors or with my family, I like
+						to read and solve puzzles.  I have an avid interest in nutrition and health.</p>
+
+
+
+				</div><!--column-->
+			</div><!--row-->
+		</div><!--container-->
+			<!--form goes here-->
+			<!--Begin Contact Form-->
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-12">
+			<form id="contact-form" action="php/mailer.php" method="post">
+				<h3 class="text-center">Contact Me</h3>
+				<div class="form-group">
+					<label for="name">Name <span class="text-danger">*</span></label>
+					<div class="input-group">
+						<div class="input-group-addon">
+							<i class="fa fa-user" aria-hidden="true"></i>
+						</div>
+						<input type="text" class="form-control" id="name" name="name" placeholder="Name">
+					</div>
+				</div>
+				<div class="form-group">
+					<label for="email">Email <span class="text-danger">*</span></label>
+					<div class="input-group">
+						<div class="input-group-addon">
+							<i class="fa fa-envelope" aria-hidden="true"></i>
+						</div>
+						<input type="email" class="form-control" id="email" name="email" placeholder="Email">
+					</div>
+				</div>
+				<div class="form-group">
+					<label for="subject">Subject</label>
+					<div class="input-group">
+						<div class="input-group-addon">
+							<i class="fa fa-pencil" aria-hidden="true"></i>
+						</div>
+						<input type="text" class="form-control" id="subject" name="subject" placeholder="Subject">
+					</div>
+				</div>
+				<div class="form-group">
+					<label for="message">Message <span class="text-danger">*</span></label>
+					<div class="input-group">
+						<div class="input-group-addon">
+							<i class="fa fa-comment" aria-hidden="true"></i>
+						</div>
+						<textarea class="form-control" rows="5" id="message" name="message" placeholder="Message (2000 characters max)"></textarea>
+					</div>
+				</div>
+
+				<!-- reCAPTCHA -->
+				<div class="g-recaptcha" data-sitekey="6Lc-9C4UAAAAAPK8J7foJo4DhihuFaHlER1fwBCM"></div>
+
+				<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Send</button>
+				<button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i> Reset</button>
+			</form>
+					</div><!--column-->
+					</div><!--row-->
+			</div><!--container-->
+
+			<!--empty area for form error/success output-->
+			<div class="row">
+				<div class="col-xs-12">
+					<div id="output-area"></div>
+				</div>
+			</div>
+		</div><!--sfooter-content-->
+		<footer>&copy; 2017 Leonora Sanchez-Rees</footer>
+
+	</body>
+</html>
---
+++ public_html/portfolio.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en" >
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+		<!-- Font Awesome -->
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
+		<!-- custom css link -->
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+
+
+
+		<!--font links -->
+
+
+		<link href="https://fonts.googleapis.com/css?family=Courgette" rel="stylesheet">
+
+
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!--[if lt IE 9]>
+		<script type="text/javascript" src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script type="text/javascript" src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!-- jquery v3.0 -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+		<!-- jQuery Form, Additional Methods, Validate -->
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.min.js"></script>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js"></script>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/additional-methods.min.js"></script>
+
+		<!-- Your JavaScript Form Validator -->
+		<script src="js/form-validate.js"></script>
+
+		<!-- Google reCAPTCHA -->
+		<script src='https://www.google.com/recaptcha/api.js'></script>
+		<title>Portfolio</title>
+	</head>
+	<body class="sfooter">
+		<div class="sfooter-content">
+			<!--added header for external links-->
+			<header>
+				<nav class="navbar navbar-default" id="top-of-page">
+					<div class="container-fluid">
+						<!-- Brand and toggle get grouped for better mobile display -->
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+							<a class="navbar-brand" href="#">Leonora Sanchez-Rees</a>
+						</div>
+						<!-- Collect the nav links, forms, and other content for toggling -->
+						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+							<ul class="nav navbar-nav navbar-right">
+								<li><a href="index2.php">Home</a></li>
+								<li><a href="#services">Services</a></li>
+								<li><a href="#information">Information</a></li>
+								<li><a href="#contact-form">Contact</a></li>
+								<li><a href="portfolio.php">Portfolio</a></li>
+
+								<li><a href="https://www.linkedin.com/in/leonora-sanchez-815aaa104/" target="_blank"><i class="fa fa-linkedin-square" aria-hidden="true" ></i></a></li>
+								<li><a href="https://github.com/lsanchezrees" target="_blank"><i class="fa fa-github" aria-hidden="true"></i></a></li>
+								<li><a href="docs/lsanchezrees-resume.pdf" target="_blank">Resum&#233;</a></li>
+							</ul>
+						</div><!-- /.navbar-collapse -->
+					</div><!-- /.container-fluid -->
+				</nav>
+			</header>
+			<h2>Coming Soon</h2>
+
+
+		</div><!--sfooter-content-->
+		<footer><p><a class="up-arrow" href="#top-of-page" data-toggle="tooltip" title="TO TOP">
+					<span class="glyphicon glyphicon-chevron-up"></span>
+				</a></p>
+			<p>&copy; 2017 Leonora Sanchez-Rees</p></footer>
+	</body>
+</html>
